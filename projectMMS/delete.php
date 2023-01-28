96e7715c2dbaeed4288f791904a3b1f11d6b1e4b
apipmuhd <123625453+apipmuhd@users.noreply.github.com>
2023-01-29 05:19:58 +0800

    Add files via upload

---
+++ projectMMS/delete.php
@@ -0,0 +1,11 @@
+<?php
+include 'config.php';
+include 'menu.php';
+$sql = "DELETE FROM user_form  WHERE id='" . $_GET["id"] . "'";
+if (mysqli_query($conn, $sql)) {
+    echo "Record deleted successfully";
+} else {
+    echo "Error deleting record: " . mysqli_error($conn);
+}
+mysqli_close($conn);
+?>
